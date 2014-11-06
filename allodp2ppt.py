e4da88add1605a3bfa0fe43879913fc1cf9fe4de
universaldilettant <universaldilettant@gmail.com>
2014-11-06 10:44:22 +0100

    added odp to pdf, png, ppt batch converter

---
+++ allodp2ppt.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import os
+import fnmatch
+import argparse
+
+
+pattern = '*.odp'
+searchpath = '/home/supportstick/pr/Scientific/'
+overwrite=False
+
+parser = argparse.ArgumentParser(description='Batch convert new or changed odp files to ppt and pdf')
+parser.add_argument('-f', action='store_true', default=False, help='force overwrite existing files')
+args = parser.parse_args()
+overwrite = args.f
+
+for root, dirnames, filenames in os.walk(searchpath):
+	for filename in fnmatch.filter(filenames, pattern):
+		filename = os.path.join(root, filename)
+		#PowerPoint Version
+		if not (os.path.exists(os.path.splitext(filename)[0]+'.ppt')) or (overwrite):
+			print "generating: "+ os.path.splitext(filename)[0]+'.ppt'
+			os.popen('/home/steffen/src/unoconv/unoconv -f ppt -o "'+root+'" "'+filename+'"')
+		else:
+			if os.path.getmtime(os.path.splitext(filename)[0]+'.ppt') < os.path.getmtime(os.path.splitext(filename)[0]+'.odp'):
+				print "updating: "+ os.path.splitext(filename)[0]+'.ppt'
+				os.popen('/home/steffen/src/unoconv/unoconv -f ppt -o "'+root+'" "'+filename+'"')
+		# PDF Version
+		if not (os.path.exists(os.path.splitext(filename)[0]+'.pdf')) or (overwrite):
+			os.popen('/home/steffen/src/unoconv/unoconv -f pdf -o "'+root+'" "'+filename+'"')
+			print "generating: "+ os.path.splitext(filename)[0]+'.pdf'
+		else:
+			if os.path.getmtime(os.path.splitext(filename)[0]+'.pdf') < os.path.getmtime(os.path.splitext(filename)[0]+'.odp'):
+				print "updating: "+ os.path.splitext(filename)[0]+'.pdf'
+				os.popen('/home/steffen/src/unoconv/unoconv -f pdf -o "'+root+'" "'+filename+'"')
+		# PNG Version
+		if not (os.path.exists(os.path.splitext(filename)[0]+'.png')) or (overwrite):
+			os.popen('/home/steffen/src/unoconv/unoconv -f png -o "'+root+'" "'+filename+'"')
+			print "generating: "+ os.path.splitext(filename)[0]+'.png'
+		else:
+			if os.path.getmtime(os.path.splitext(filename)[0]+'.png') < os.path.getmtime(os.path.splitext(filename)[0]+'.odp'):
+				print "updating: "+ os.path.splitext(filename)[0]+'.png'
+				os.popen('/home/steffen/src/unoconv/unoconv -f png -o "'+root+'" "'+filename+'"')
